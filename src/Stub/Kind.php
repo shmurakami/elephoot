91db99cefe7ac01edca1f61af0c05c42d700ce5a
shaka <shmurakami1104@gmail.com>
2022-02-06 23:18:49 +0900

    update ast stub to v1.0.16

--- src/Stub/Kind.php
+++ src/Stub/Kind.php
@@ -1,19 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace shmurakami\Elephoot\Stub;
 
 /**
- * @see https://github.com/nikic/php-ast/blob/v1.0.2/ast_stub.php obtained by github
+ * @see https://github.com/nikic/php-ast/blob/v1.0.16/ast_stub.php obtained by github
+ * PHPDoc stub file for ast extension
+ * @author Bill Schaller <bill@zeroedin.com>
+ * @author Nikita Popov <nikic@php.net>
  */
 class Kind
 {
-    const AST_MAGIC_CONST = 0;
-    const AST_TYPE = 1;
-    const AST_FUNC_DECL = 67;
-    const AST_CLOSURE = 68;
-    const AST_METHOD = 69;
-    const AST_CLASS = 70;
     const AST_ARG_LIST = 128;
+    const AST_LIST = 255;
     const AST_ARRAY = 129;
     const AST_ENCAPS_LIST = 130;
     const AST_EXPR_LIST = 131;
@@ -29,7 +29,22 @@ class Kind
     const AST_NAME_LIST = 141;
     const AST_TRAIT_ADAPTATIONS = 142;
     const AST_USE = 143;
-    const AST_LIST = 255;
+    const AST_TYPE_UNION = 144;
+    const AST_TYPE_INTERSECTION = 145;
+    const AST_ATTRIBUTE_LIST = 146;
+    const AST_ATTRIBUTE_GROUP = 147;
+    const AST_MATCH_ARM_LIST = 148;
+    const AST_NAME = 2048;
+    const AST_CLOSURE_VAR = 2049;
+    const AST_NULLABLE_TYPE = 2050;
+    const AST_FUNC_DECL = 67;
+    const AST_CLOSURE = 68;
+    const AST_METHOD = 69;
+    const AST_ARROW_FUNC = 71;
+    const AST_CLASS = 70;
+    const AST_MAGIC_CONST = 0;
+    const AST_TYPE = 1;
+    const AST_CALLABLE_CONVERT = 3;
     const AST_VAR = 256;
     const AST_CONST = 257;
     const AST_UNPACK = 258;
@@ -47,59 +62,62 @@ class Kind
     const AST_POST_INC = 273;
     const AST_POST_DEC = 274;
     const AST_YIELD_FROM = 275;
-    const AST_GLOBAL = 276;
-    const AST_UNSET = 277;
-    const AST_RETURN = 278;
-    const AST_LABEL = 279;
-    const AST_REF = 280;
-    const AST_HALT_COMPILER = 281;
-    const AST_ECHO = 282;
-    const AST_THROW = 283;
-    const AST_GOTO = 284;
-    const AST_BREAK = 285;
-    const AST_CONTINUE = 286;
-    const AST_CLASS_NAME = 511;
+    const AST_GLOBAL = 277;
+    const AST_UNSET = 278;
+    const AST_RETURN = 279;
+    const AST_LABEL = 280;
+    const AST_REF = 281;
+    const AST_HALT_COMPILER = 282;
+    const AST_ECHO = 283;
+    const AST_THROW = 284;
+    const AST_GOTO = 285;
+    const AST_BREAK = 286;
+    const AST_CONTINUE = 287;
+    const AST_CLASS_NAME = 276;
+    const AST_CLASS_CONST_GROUP = 546;
     const AST_DIM = 512;
     const AST_PROP = 513;
-    const AST_STATIC_PROP = 514;
-    const AST_CALL = 515;
-    const AST_CLASS_CONST = 516;
-    const AST_ASSIGN = 517;
-    const AST_ASSIGN_REF = 518;
-    const AST_ASSIGN_OP = 519;
-    const AST_BINARY_OP = 520;
-    const AST_ARRAY_ELEM = 525;
-    const AST_NEW = 526;
-    const AST_INSTANCEOF = 527;
-    const AST_YIELD = 528;
-    const AST_STATIC = 530;
-    const AST_WHILE = 531;
-    const AST_DO_WHILE = 532;
-    const AST_IF_ELEM = 533;
-    const AST_SWITCH = 534;
-    const AST_SWITCH_CASE = 535;
-    const AST_DECLARE = 536;
-    const AST_USE_TRAIT = 537;
-    const AST_TRAIT_PRECEDENCE = 538;
-    const AST_METHOD_REFERENCE = 539;
-    const AST_NAMESPACE = 540;
-    const AST_USE_ELEM = 541;
-    const AST_TRAIT_ALIAS = 542;
-    const AST_GROUP_USE = 543;
-    const AST_PROP_GROUP = 767;
+    const AST_NULLSAFE_PROP = 514;
+    const AST_STATIC_PROP = 515;
+    const AST_CALL = 516;
+    const AST_CLASS_CONST = 517;
+    const AST_ASSIGN = 518;
+    const AST_ASSIGN_REF = 519;
+    const AST_ASSIGN_OP = 520;
+    const AST_BINARY_OP = 521;
+    const AST_ARRAY_ELEM = 526;
+    const AST_NEW = 527;
+    const AST_INSTANCEOF = 528;
+    const AST_YIELD = 529;
+    const AST_STATIC = 532;
+    const AST_WHILE = 533;
+    const AST_DO_WHILE = 534;
+    const AST_IF_ELEM = 535;
+    const AST_SWITCH = 536;
+    const AST_SWITCH_CASE = 537;
+    const AST_DECLARE = 538;
+    const AST_PROP_ELEM = 775;
+    const AST_PROP_GROUP = 774;
+    const AST_CONST_ELEM = 776;
+    const AST_USE_TRAIT = 539;
+    const AST_TRAIT_PRECEDENCE = 540;
+    const AST_METHOD_REFERENCE = 541;
+    const AST_NAMESPACE = 542;
+    const AST_USE_ELEM = 543;
+    const AST_TRAIT_ALIAS = 544;
+    const AST_GROUP_USE = 545;
+    const AST_ATTRIBUTE = 547;
+    const AST_MATCH = 548;
+    const AST_MATCH_ARM = 549;
+    const AST_NAMED_ARG = 550;
     const AST_METHOD_CALL = 768;
-    const AST_STATIC_CALL = 769;
-    const AST_CONDITIONAL = 770;
-    const AST_TRY = 771;
-    const AST_CATCH = 772;
-    const AST_PARAM = 773;
-    const AST_PROP_ELEM = 774;
-    const AST_CONST_ELEM = 775;
+    const AST_NULLSAFE_METHOD_CALL = 769;
+    const AST_STATIC_CALL = 770;
+    const AST_CONDITIONAL = 771;
+    const AST_TRY = 772;
+    const AST_CATCH = 773;
     const AST_FOR = 1024;
     const AST_FOREACH = 1025;
-    const AST_ARROW_FUNC = 1535;
-    const AST_NAME = 2048;
-    const AST_CLOSURE_VAR = 2049;
-    const AST_NULLABLE_TYPE = 2050;
-
+    const AST_ENUM_CASE = 1026;
+    const AST_PARAM = 1280;
 }
